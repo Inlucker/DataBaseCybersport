d4faa5063845f513beb385b92207f84bcb530d87
Inlucker <arseny707@mail.ru>
2022-06-13 01:27:41 +0300

    updated Roles

--- SQLScripts/Roles.sql
+++ SQLScripts/Roles.sql
@@ -26,14 +26,31 @@ grant update on users_id_seq to guest;
 --Создание team_captain
 CREATE USER team_captain password 'team_captain';
 --Добавление прав team_captain
-grant postgres to team_captain;
+--grant postgres to team_captain;
+grant pg_read_all_data to team_captain;
+grant update on Players to team_captain;
+grant insert, update on Teams to team_captain;
+grant update on teams_id_seq to team_captain;
+grant delete on Users to team_captain;
 
 --Создание studio_owner
 CREATE USER studio_owner password 'studio_owner';
 --Добавление прав studio_owner
-grant postgres to studio_owner;
+--grant postgres to studio_owner;
+grant pg_read_all_data to studio_owner;
+grant update on Commentators to studio_owner;
+grant insert, update on Studios to studio_owner;
+grant update on studios_id_seq to studio_owner;
+grant delete on Users to studio_owner;
 
 --Создание tournament_organizer
 CREATE USER tournament_organizer password 'tournament_organizer';
 --Добавление прав tournament_organizer
-grant postgres to tournament_organizer;
+--grant postgres to tournament_organizer;
+grant pg_read_all_data to tournament_organizer;
+grant insert, update on Tournaments to tournament_organizer;
+grant update on tournaments_id_seq to tournament_organizer;
+grant insert on TournamentTeams to tournament_organizer;
+grant insert, update on Matches to tournament_organizer;
+grant update on matches_id_seq to tournament_organizer;
+grant delete on Users to tournament_organizer;
