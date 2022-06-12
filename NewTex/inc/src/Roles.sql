7b94bb5826a3896c30cbfc1ea80eb8f48a00583a
Inlucker <arseny707@mail.ru>
2022-06-13 01:32:26 +0300

    updated RPZ with updated Roles

--- NewTex/inc/src/Roles.sql
+++ NewTex/inc/src/Roles.sql
@@ -1,21 +1,31 @@
 --guest
 CREATE USER guest password 'guest';
---granting access rights to guest
 grant pg_read_all_data to guest;
 grant insert on users to guest;
 grant update on users_id_seq to guest;
 
 --team_captain
 CREATE USER team_captain password 'team_captain';
---granting access rights to team_captain
-grant postgres to team_captain;
+grant pg_read_all_data to team_captain;
+grant update on Players to team_captain;
+grant insert, update on Teams to team_captain;
+grant update on teams_id_seq to team_captain;
+grant delete on Users to team_captain;
 
 --studio_owner
 CREATE USER studio_owner password 'studio_owner';
---granting access rights to studio_owner
-grant postgres to studio_owner;
+grant pg_read_all_data to studio_owner;
+grant update on Commentators to studio_owner;
+grant insert, update on Studios to studio_owner;
+grant update on studios_id_seq to studio_owner;
+grant delete on Users to studio_owner;
 
 --tournament_organizer
 CREATE USER tournament_organizer password 'tournament_organizer';
---granting access rights to tournament_organizer
-grant postgres to tournament_organizer;
+grant pg_read_all_data to tournament_organizer;
+grant insert, update on Tournaments to tournament_organizer;
+grant update on tournaments_id_seq to tournament_organizer;
+grant insert on TournamentTeams to tournament_organizer;
+grant insert, update on Matches to tournament_organizer;
+grant update on matches_id_seq to tournament_organizer;
+grant delete on Users to tournament_organizer;
